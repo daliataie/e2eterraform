eee2d0adf418f38264429676850dabf3b6921a00
daliataie <dalia_altaie@yahoo.com>
2021-08-24 21:01:13 -0500

    new credantial

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA3CHQPH7BVVS4SKG5"
-  secret_key = "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"
+  access_key = "AKIA3CHQPH7BUAQA52PE"
+  secret_key = "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"
 }
